02add4e059a5e7d8d6c545e610c2699dfdb08213
Hristo Karchev <x_karchev@mail.bg>
2021-09-05 00:02:00 +0300

    Questions css and page started
    + data is not from API

--- client/src/components/Questions/Questions.js
+++ client/src/components/Questions/Questions.js
@@ -1,12 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Questions.css';
+//import axios from './axios';
 
-const Questions = function Start(props) {
+const Questions = function Questions() {
+    /* const [questions, setQuestion] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get("/questions/all");
+
+            setQuestion(req.data);
+        }
+
+        fetchData();
+    }, []); */
+
+    const questions = [
+        {
+            questionText: 'What is the capital of France?',
+            answerOptions: [
+                { answerText: 'New York', isCorrect: false },
+                { answerText: 'London', isCorrect: false },
+                { answerText: 'Paris', isCorrect: true },
+                { answerText: 'Dublin', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'Who is CEO of Tesla?',
+            answerOptions: [
+                { answerText: 'Jeff Bezos', isCorrect: false },
+                { answerText: 'Elon Musk', isCorrect: true },
+                { answerText: 'Bill Gates', isCorrect: false },
+                { answerText: 'Tony Stark', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'The iPhone was created by which company?',
+            answerOptions: [
+                { answerText: 'Apple', isCorrect: true },
+                { answerText: 'Intel', isCorrect: false },
+                { answerText: 'Amazon', isCorrect: false },
+                { answerText: 'Microsoft', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'How many Harry Potter books are there?',
+            answerOptions: [
+                { answerText: '1', isCorrect: false },
+                { answerText: '4', isCorrect: false },
+                { answerText: '6', isCorrect: false },
+                { answerText: '7', isCorrect: true },
+            ],
+        },
+    ];
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const handleAnswerOptionClick = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
+
+    /* console.log(questions[1].answerOptions); */
+
     return (
-        <div>
-            <h1>YOOO Question PAGE</h1>
+        <div className='question__page'>
+            <div className='questions'>
+                {showScore ? (
+                    <div className='score-section'>
+                        You scored {score} out of {questions.length}
+                        <Link to="/"><button className="start__quiz">Try again?</button></Link>
+                    </div>
+                ) : (
+                    <>
+                       {/*  <div className="round-time-bar">
+                            
+                        </div> */}
+                        
+                        <div className='question__section'>
+                            {/* <div className='question-count'>
+                                <span>Question {currentQuestion + 1}</span>/{questions.length}
+                            </div> */}
+                            <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                            {/* <div className='question-text'>{questions.map((question) => (question[currentQuestion].questionText))}</div> */}
+                        </div>
+                        <div className='answer-section'>
+                            {questions[currentQuestion].answerOptions.map((answerOption) => (
+                                <button className="question__btn" onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                            ))}
+                        </div>
+                        {/* <div className='answer-section'>
+						{questions.map((question) => (question[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))))}
+					</div> */}
+                    </>
+                )}
+            </div>
         </div>
-    )
+    );
 }
 
 
