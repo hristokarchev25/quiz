905fb360ad685ba52d1444f8a2fd26501bac116c
Hristo Karchev <x_karchev@mail.bg>
2021-09-06 21:01:04 +0300

    Updated Questions.js

--- client/src/components/Questions/Questions.js
+++ client/src/components/Questions/Questions.js
@@ -5,8 +5,6 @@ import axios from '../../axios';
 
 const Questions = function Questions() {
     const [questions, setQuestion] = useState([]);
-    /*  questions.map((question) => console.log(question.questionText));
-     console.log(questions); */
     useEffect(() => {
         async function fetchData() {
             const req = await axios.get("/questions/all");
@@ -17,46 +15,6 @@ const Questions = function Questions() {
 
         fetchData();
     }, []);
-
-    /*  const questions = [
-         {
-             questionText: 'What is the capital of France?',
-             answerOptions: [
-                 { answerText: 'New York', isCorrect: false },
-                 { answerText: 'London', isCorrect: false },
-                 { answerText: 'Paris', isCorrect: true },
-                 { answerText: 'Dublin', isCorrect: false },
-             ],
-         },
-         {
-             questionText: 'Who is CEO of Tesla?',
-             answerOptions: [
-                 { answerText: 'Jeff Bezos', isCorrect: false },
-                 { answerText: 'Elon Musk', isCorrect: true },
-                 { answerText: 'Bill Gates', isCorrect: false },
-                 { answerText: 'Tony Stark', isCorrect: false },
-             ],
-         },
-         {
-             questionText: 'The iPhone was created by which company?',
-             answerOptions: [
-                 { answerText: 'Apple', isCorrect: true },
-                 { answerText: 'Intel', isCorrect: false },
-                 { answerText: 'Amazon', isCorrect: false },
-                 { answerText: 'Microsoft', isCorrect: false },
-             ],
-         },
-         {
-             questionText: 'How many Harry Potter books are there?',
-             answerOptions: [
-                 { answerText: '1', isCorrect: false },
-                 { answerText: '4', isCorrect: false },
-                 { answerText: '6', isCorrect: false },
-                 { answerText: '7', isCorrect: true },
-             ],
-         },
-     ];
-  */
 
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [showScore, setShowScore] = useState(false);
@@ -81,10 +39,6 @@ const Questions = function Questions() {
             document.querySelector("time__line").style.width = time + "px";
         }
     };
-    /* console.log(questions[1].answerOptions); */
-    /*  questions.map((question) => console.log(question.questionText));
-     console.log(currentQuestion);
-     console.log(questions); */
 
     return (
         <div className='question__page'>
@@ -99,16 +53,8 @@ const Questions = function Questions() {
                 ) : (
                     <>
                         <div className='question__section'>
-                            {/* <div className='question__text'>{questions[currentQuestion].questionText}</div> */}
-                            {/* <div className='question__text'>{questions.map((question) => (question[currentQuestion].questionText))}</div> */}
                             <div className='question__text'>{questions[currentQuestion].questionText}</div>
                         </div>
-                        {/*  <div className='answer__section'>
-                            {questions[currentQuestion].answerOptions.map((answerOption) => (
-                                <button className="question__btn" onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}><span className="answer__prefix">&#65;</span>{answerOption.answerText}</button>
-                            ))}
-                            
-                        </div> */}
                         <div className='answer__section'>
                             {questions[currentQuestion].answerOptions.map((answerOption) => (
                                 <button className="question__btn" onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}><span className="answer__prefix">{answerOption.answerPrefix}</span>{answerOption.answerText}</button>
